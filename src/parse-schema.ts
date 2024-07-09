9701ae2000597e6865069f2a35641fc4a41fdf0d
Fúlvio Carvalhido <fulvio@diller.no>
2024-07-09 16:59:25 +0100

    Fix Tests

--- src/parse-schema.ts
+++ src/parse-schema.ts
@@ -1,12 +1,12 @@
-import jsYaml from 'js-yaml'
-import { JSONSchema } from 'json-schema-to-typescript';
-import $RefParser from '@apidevtools/json-schema-ref-parser';
-import { readFileSync } from 'fs';
-import { dirname } from 'path';
+import $RefParser from "@apidevtools/json-schema-ref-parser";
+import { readFileSync } from "node:fs";
+import jsYaml from "js-yaml";
+import type { JSONSchema } from "json-schema-to-typescript";
+import { dirname } from "node:path";
 
-const toJsonSchema = require('@openapi-contrib/openapi-schema-to-json-schema');
+import toJsonSchema from "@openapi-contrib/openapi-schema-to-json-schema";
 
-export type SchemaType = 'yaml' | 'json' | 'custom';
+export type SchemaType = "yaml" | "json" | "custom";
 
 export interface ParsedSchema {
   json: string;
@@ -14,22 +14,28 @@ export interface ParsedSchema {
   whitelistedDecoders: string[] | undefined;
 }
 
-export async function parseSchema(inputFilePath: string, schemaType: SchemaType): ParsedSchema {
+export async function parseSchema(
+  inputFilePath: string,
+  schemaType: SchemaType
+): Promise<ParsedSchema> {
   switch (schemaType) {
-    case 'json':
-    case 'yaml':
+    case "json":
+    case "yaml":
       return parseOpenApiSchema(inputFilePath, schemaType);
-    case 'custom':
+    case "custom":
       return parseCustomSchema(inputFilePath);
   }
 }
 
-async function parseOpenApiSchema(inputFilePath: string, schemaType: 'yaml' | 'json'): ParsedSchema {
+async function parseOpenApiSchema(
+  inputFilePath: string,
+  schemaType: "yaml" | "json"
+): Promise<ParsedSchema> {
   let schema: any;
 
-  const inputFileContent = readFileSync(inputFilePath, 'utf8');
+  const inputFileContent = readFileSync(inputFilePath, "utf8");
 
-  if (schemaType === 'yaml') {
+  if (schemaType === "yaml") {
     schema = jsYaml.load(inputFileContent);
   } else {
     schema = JSON.parse(inputFileContent);
@@ -38,61 +44,66 @@ async function parseOpenApiSchema(inputFilePath: string, schemaType: 'yaml' | 'j
   const originalDirectory = process.cwd();
   process.chdir(dirname(inputFilePath));
   // resolve external references to original schema
-  schema = await $RefParser.bundle(schema)
+  schema = await $RefParser.bundle(schema);
   // change back to original directory
   process.chdir(originalDirectory);
 
   const properties: Record<string, any> = {};
-  const definitions: Record<string, JSONSchema> = {};
+  const definitions: Record<string, any> = {};
 
-  Object.entries(schema.components.schemas).forEach(([key, value]) => {
+  for (const [key, value] of Object.entries(schema.components.schemas)) {
     properties[key] = { $ref: `#/definitions/${key}` };
-    definitions[key] = toJsonSchema(value);
-  });
+    definitions[key] = toJsonSchema(value as any);
+  }
 
   // open api is a bit different so we need to creata a different schema
-  const schemaJsonOutput = JSON
-    .stringify({
-      type: 'object',
-      title: 'Schema',
+  const schemaJsonOutput = JSON.stringify(
+    {
+      type: "object",
+      title: "Schema",
       definitions,
       properties,
-    }, undefined, 2)
-    .replace(/\#\/components\/schemas/g, '#/definitions')
+    },
+    undefined,
+    2
+  ).replace(/\#\/components\/schemas/g, "#/definitions");
 
   return {
     json: schemaJsonOutput,
     definitions,
     whitelistedDecoders: undefined,
-  }
+  };
 }
 
 function parseCustomSchema(inputFilePath: string): ParsedSchema {
   const schema = require(inputFilePath);
 
-  if (typeof schema.types !== 'object') {
+  if (typeof schema.types !== "object") {
     throw new Error('schema "types" should be an object');
   }
 
   const properties: Record<string, any> = {};
-  const definitions: Record<string, any> = {}
+  const definitions: Record<string, any> = {};
 
-  Object.entries(schema.types).forEach(([key, value]) => {
+  for (const [key, value] of Object.entries(schema.types)) {
     properties[key] = { $ref: `#/definitions/${key}` };
     definitions[key] = value;
-  });
+  }
 
-  const schemaJsonOutput = JSON
-    .stringify({
-      type: 'object',
-      title: 'Schema',
+  const schemaJsonOutput = JSON.stringify(
+    {
+      type: "object",
+      title: "Schema",
       definitions,
       properties,
-    }, undefined, 2);
+    },
+    undefined,
+    2
+  );
 
   return {
     json: schemaJsonOutput,
     definitions,
     whitelistedDecoders: schema.decoders,
-  }
+  };
 }
--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   automock: false,
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/generated', '<rootDir>/build'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  modulePathIgnorePatterns: ["<rootDir>/generated", "<rootDir>/build"],
   verbose: true,
 };
